6706443c54f6d38c91d582ddddb3552860370e56
jeffresh <manuel.pallanunez@alum.uca.es>
2020-11-06 09:23:55 +0100

    :sparkles: add function to compute perceptron output

---
+++ src/compute_perceptron.m
@@ -0,0 +1,8 @@
+function computed_output  = compute_perceptron(w,x, activation_function)
+%ACTIVATION_FUNCTION Summary of this function goes here
+%   Detailed explanation goes here
+
+computed_output = activation_function(w,x);
+
+end
+
